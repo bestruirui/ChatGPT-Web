8d15f289e2a2e44a7b40f9c283cee79c6bd60115
小瑞 <89796393+bestruirui@users.noreply.github.com>
2023-01-09 14:18:48 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -109,7 +109,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://proxy-one-murex.vercel.app', {
+    const response = await fetch('http://chatgptapi.bestrui.top', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
